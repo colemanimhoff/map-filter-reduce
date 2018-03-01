2f7775db793ddf612fe1ada37a308cf7e77f9725
matt-winzer <matt.winzer@gmail.com>
2018-03-01 13:01:28 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Native Array Methods: Map, Filter, Reduce
+
+## Objectives
+
+* Explain the use case for native array methods
+  - map
+  - filter
+  - reduce
+* Write map, filter, and reduce functions
+* Identify the parts of a native array method
+
+## Notes
---
+++ app.js
@@ -0,0 +1,32 @@
+const spells = require('./spells').spells
+
+
+// MAP
+
+// Clean up the data so it's more readable
+// Let's map through the array, and pull out each spell's name, desc, and duration
+
+// CODE TOGETHER HERE
+
+
+// Write a function called cleanSpells
+  // Takes an array of spells as an input
+  // Returns a new array of spells that only includes:
+    // Spell name
+    // Spell material
+    // Name of the first class that can use the spell
+
+
+// FILTER
+
+// Write a function called filterSpellsLevel that returns a new array of spells
+  // Spell's level is no higher than 1
+  // Keep spells that are level 1 or below
+  // Remove spells that are level 2 or above
+
+
+// REDUCE
+
+// Write a function called reduceSpellNames that returns an array of the spells' names
+  // Function takes an array of spell objects as an input
+  // Function returns an array of spell names as an output
