2ae630731e3c38d012375e1e947bd11820632bd7
matt-winzer <matt.winzer@gmail.com>
2018-03-01 14:15:39 -0700

    completed map, filter

--- README.md
+++ README.md
@@ -8,5 +8,30 @@
   - reduce
 * Write map, filter, and reduce functions
 * Identify the parts of a native array method
+* Passing a named function to native method
+* Chaining or using multiple at once
 
 ## Notes
+
+- forEach
+  - Basically just replaces FOR loop
+  
+- Use case for MAP
+  - Take an array of data and transform each index in the same way
+  - Return a new array from the existing arrays
+  - Difference from forEach?
+  - Often 'cleaning up' the data
+  - ALWAYS return an array of the SAME length
+  - MUST include a return statement
+
+- Use case for FILTER
+  - Return a new array that only includes things that pass our 'test'
+  - We're evaluating against some condition
+  - Check every item/index in the array against that condition
+  - New array will be of EQUAL or LESSER length than input
+
+- Use case for REDUCE
+  - Takes all items in array and reduces to single value
+  - Swiss Army knife of NAM
+  - Not necessarily a new array
+  - Could be array, object, number, string, etc.
--- app.js
+++ app.js
@@ -8,6 +8,22 @@ const spells = require('./spells').spells
 
 // CODE TOGETHER HERE
 
+function simpleSpells(spells) {
+  let cleanSpells = spells.map(function(spell) {
+    // Business Logic
+
+    let cleanSpell = {
+      name: spell.name,
+      description: spell.desc[0],
+      duration: spell.duration
+    }
+
+    return cleanSpell
+  })
+
+  return cleanSpells
+}
+
 
 // Write a function called cleanSpells
   // Takes an array of spells as an input
@@ -16,6 +32,20 @@ const spells = require('./spells').spells
     // Spell material
     // Name of the first class that can use the spell
 
+function cleanSpells(spells) {
+  let limitedSpells = spells.map(function(spell) {
+    let firstClass = spell.classes[0].name;
+    let limitedSpell = {
+      name: spell.name,
+      material: spell.material,
+      firstClass: spell.classes[0].name
+    };
+    return limitedSpell;
+  });
+  return limitedSpells;
+}
+
+// console.log(cleanSpells(spells))
 
 // FILTER
 
@@ -24,6 +54,21 @@ const spells = require('./spells').spells
   // Keep spells that are level 1 or below
   // Remove spells that are level 2 or above
 
+function filterSpellsLevel(spells) {
+  let lowSpells = spells.filter(function(spell) {
+
+    // Test or Condition
+    if (spell.level < 2) {
+      return spell
+    }
+
+    return spell.level < 2;
+
+  });
+  return lowSpells;
+}
+
+console.log(filterSpellsLevel(spells))
 
 // REDUCE
 
